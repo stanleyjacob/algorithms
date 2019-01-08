930978879e70907f8ee1c82741c19d47f56b6eeb
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-01-07 23:59:23 -0800

    Update generate_parenthesis.py

--- generate_parenthesis.py
+++ generate_parenthesis.py
@@ -1,14 +1,19 @@
 
+# returns possible combinations
+# of parenthesis given n pairs
 def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
         if n == 0:
             return
         if n == 1:
             return ['()']
         max_num_open_parenthesis = n / 2
         
+        parenthesis_list = []
         for i in range(n):
-            pass
+                for i in range(n):
+                        if i == 0:
+                                parenthesis_list.append('(')
+                        elif i == (n - 1):
+                                parenthesis_list.append(')')
+                        else:
+                                pass
