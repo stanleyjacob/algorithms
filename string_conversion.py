fb6b68cfe391ff196734a0a5b06b0b9b5c90d757
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-01-09 00:30:11 -0800

    Update string_conversion.py

--- string_conversion.py
+++ string_conversion.py
@@ -1,11 +1,30 @@
-
 def convert(s, numRows):
-  total_len = len(s) / numRows
-  str_matrix = [['\0' for j in range(numRows)] for i in range(total_len)]
-  ind = 0
-  column_count = 0
-  for i in range(len(s)):
-    matrix_size[ind][column_count] = s[i]
-    ind = (ind + 1) % numRows
-    column_count += 1
-  print(str_matrix)
+  total_len = len(s)
+	#print(total_len)
+	str_matrix = [['' for j in range(total_len)] for i in range(numRows)]
+	ind = 0
+	column_count = 0
+	i = 0
+	while i != len(s):
+		if column_count % 2 == 1 and ind % 2 == 0:
+			ind += 1
+			if ind == numRows:
+				ind = 0
+				column_count += 1
+			continue
+		str_matrix[ind][column_count] = s[i]
+		i += 1
+		ind += 1
+		if ind == numRows:
+			ind = 0
+			column_count += 1
+
+	final_str = []
+	for i in range(numRows):
+		#print(str_matrix[i])
+		for j in range(len(str_matrix[i])):
+			current_char = str_matrix[i][j]
+			#print(current_char)
+			if current_char != '':
+				final_str.append(current_char)
+	return ''.join(final_str)
