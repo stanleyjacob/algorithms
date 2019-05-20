740ecefa00045c2ede836129887caa92d8f97dd4
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-05-19 22:44:52 -0700

    Update invert_binary_tree.py

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -1,4 +1,5 @@
 
+# O(d) space, O(n) time
 def invertBinaryTree(tree):
     if tree is None:
         return
@@ -7,3 +8,18 @@ def invertBinaryTree(tree):
     tree.right = temp
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
+
+# O(n) space, O(n) time
+# Common bug: referring to tree in while loop
+#             rather than curr_elem
+def invertBinaryTree(tree):
+	curr_queue = [tree]
+	while len(curr_queue):
+		curr_elem = curr_queue.pop(0)
+		if curr_elem is None:
+			continue
+		else:
+			curr_elem.left, curr_elem.right = \
+				curr_elem.right, curr_elem.left
+			curr_queue.append(curr_elem.left)
+			curr_queue.append(curr_elem.right)
