209d3b8d428eaff7274bd7ccb48cd1b1fc0c35ee
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-06-20 16:00:50 -0700

    Update lcs_recursive.py

--- lcs_recursive.py
+++ lcs_recursive.py
@@ -9,7 +9,7 @@ def lcs_length(string1, string2, int m, int n):
     return lcs_length(string1, string2, m - 1, n - 1) + 1
   
   return max(lcs_length(string1, string2, m, n - 1), \
-    lcs_length(string1, string2, m - 1, n)
+    lcs_length(string1, string2, m - 1, n))
   
 def main():
   x = 'ser'
