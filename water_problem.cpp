a59b2e290cfdb5e9df4b15fdc70a3496e242faeb
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-07-02 23:50:02 -0700

    Create water_problem.cpp

---
+++ water_problem.cpp
@@ -0,0 +1,12 @@
+
+#include <vector>
+using namespace std;
+
+// general idea:
+// keep track of max elem left of curr elem
+// keep track of max elem right of curr elem
+// (3) keep track of amount of water trapped at current index
+// return sum of (3)
+int waterArea(vector<int> heights) {
+	
+}
