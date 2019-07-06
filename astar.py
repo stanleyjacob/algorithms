38a169ae6cb7d5506dcf95a7415d8af392113155
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-07-06 07:08:34 -0700

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,1 +1,16 @@
 
+
+def reconstruct(source_dict, curr_index):
+    total_path = [curr_index]
+    while curr_index in source_dict.keys():
+        curr_index = source_dict[curr_index]
+        total_path.append(curr_index)
+    return total_path
+  
+def a_star(start, goal):
+  
+    evaluated_set = {}
+    needs_evaluation_set = {start}
+    
+    previous_map = {}
+    
