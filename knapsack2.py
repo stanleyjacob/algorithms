3ab8ddf5b3111e4d10459846ecb2e34fe913e201
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-07-05 22:45:19 -0700

    Create knapsack2.py

---
+++ knapsack2.py
@@ -0,0 +1,10 @@
+
+class Item:
+    def __init__(self, weight, individual_value):
+        self.weight = weight
+        self.value = individual_value
+
+# given a list of Items, and a limit cap
+# construct DP table
+def knapsackProblem(items, cap):
+    pass
