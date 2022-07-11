d0f03ddcd2ad3d7f64cdd78bd5aeb4ace1e402f7
Stanley Jacob <stanleyjacob@users.noreply.github.com>
2022-07-10 23:14:42 -0700

    Update max_subarray.cpp

--- max_subarray.cpp
+++ max_subarray.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int max_sum = INT_MIN;
+        int curr_sum = 0;
+        // unordered_map<pair<int, int>, int> range_sum_map;
         for (int start_ind = 0; start_ind < nums.size(); ++start_ind) {
-            for (int end_ind = start_ind; end_ind < nums.size(); ++ end_ind) {
-                accumulate(nums.begin() + start_ind, nums.begin() + end_ind, 
-                          decltype(nums)::value_type(0));
-            }
+            curr_sum = max(curr_sum + nums[start_ind], nums[start_ind]);
+            max_sum = max(curr_sum, max_sum);
         }
         return max_sum;
     }
